83a84a6b7591e4abef6f0a398545265ae20f2b4d
Nithima44 <nitima7859@gmail.com>
2026-03-25 11:21:06 +0700

    1

--- src/products/product.controller.ts
+++ src/products/product.controller.ts
@@ -5,7 +5,7 @@ import { ProductService } from './product.service';
 export class ProductController {
   constructor(private readonly productService: ProductService) {}
 
-  @Get()
+  @Get('products')
   getProducts() {
     return {
       success: true,
