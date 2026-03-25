2288dab94022ef5e818c6b8b6895136ac99b7f37
Nithima44 <nitima7859@gmail.com>
2026-03-25 12:15:16 +0700

    1

--- src/products/product.service.ts
+++ src/products/product.service.ts
@@ -7,12 +7,8 @@ import * as path from 'path';
 export class ProductService {
   private readonly filePath = path.join(process.cwd(), 'data', 'products.json');
   private async readData(): Promise<Product[]> {
-    try {
-      const data = await fs.readFileSync(this.filePath, 'utf-8');
-      return JSON.parse(data) as Product[];
-    } catch (error) {
-      return [];
-    }
+    const data = await fs.readFileSync(this.filePath, 'utf-8');
+    return JSON.parse(data) as Product[];
   }
 
   async findAll(): Promise<Product[]> {
