6593403e3793998fc179d74dfe612dab93675610
Nithima44 <nitima7859@gmail.com>
2026-03-25 11:17:34 +0700

    1

--- src/products/product.service.ts
+++ src/products/product.service.ts
@@ -1,20 +1,13 @@
 import { Injectable } from '@nestjs/common';
-import * as fs from 'fs/promises';
-import * as path from 'path';
+import * as fs from 'fs';
+//import * as path from 'path';
 import { Product } from './product.interface';
 
 @Injectable()
 export class ProductService {
-  private readonly filePath = path.join(process.cwd(), 'data', 'products.json');
-
-  constructor(private readonly productService: ProductService) {}
-
-  private async readData(): Promise<Product[]> {
-    const data = await fs.readFile(this.filePath, 'utf-8');
-    return JSON.parse(data) as Product[];
-  }
-
-  async findAll(): Promise<Product[]> {
-    return await this.readData();
+  findAll(): Product[] {
+    const rawdata = fs.readFileSync('data/products.json', 'utf-8');
+    const data = JSON.parse(rawdata) as Product[];
+    return data;
   }
 }
